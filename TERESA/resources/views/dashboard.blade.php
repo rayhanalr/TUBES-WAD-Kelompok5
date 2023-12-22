a501565af46b9b8ca31133f9cf03e5d13c401dad
rayhanalr <rayhanalrasyid10@gmail.com>
2023-12-22 07:21:38 +0700

    asistensi 1

---
+++ TERESA/resources/views/dashboard.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <title>Dashboard</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins%3A400%2C500%2C600%2C700"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C500%2C600%2C700"/>
+  <link rel="stylesheet" href="./styles/dashboard.css"/>
+</head>
+<body>
+<div class="dashboard-Fho">
+  <p class="dashboard-PZ7">Dashboard</p>
+  <div class="group-5-h41">
+    <div class="rectangle-1-peR">
+    </div>
+    <p class="dian-sastrowardoyo-Nfw">Dian Sastrowardoyo</p>
+    <div class="rectangle-7-sMo">
+    </div>
+    <img class="vuesax-linear-home-2-C9B" src="./assets/vuesax-linear-home-2-sQy.png"/>
+    <p class="home-7X3">Home</p>
+    <p class="reservation-pgM">Reservation</p>
+    <img class="vuesax-linear-note-8S9" src="./assets/vuesax-linear-note-CCM.png"/>
+  </div>
+  <img class="group-2-TUR" src="./assets/group-2-46R.png"/>
+  <div class="group-4-CB7">
+    <div class="auto-group-wj1p-5Vo">
+      <p class="reject-DM7">Reject</p>
+      <p class="item-0-Xsb">0</p>
+    </div>
+    <img class="auto-group-q4ft-46q" src="./assets/auto-group-q4ft.png"/>
+  </div>
+  <div class="group-1-9PB">
+    <div class="auto-group-qmkm-E9j">
+      <p class="item-0-mvM">0</p>
+      <p class="approved-tzy">Approved</p>
+    </div>
+    <img class="auto-group-kkhb-Ckm" src="./assets/auto-group-kkhb.png"/>
+  </div>
+  <div class="group-3-uQH">
+    <div class="auto-group-x2bb-1TK">
+      <p class="waiting-kA1">Waiting</p>
+      <p class="item-0-fXs">0</p>
+    </div>
+    <img class="auto-group-ynqf-aeq" src="./assets/auto-group-ynqf.png"/>
+  </div>
+  <img class="frame-8-1-sdw" src="./assets/frame-8-1-myo.png"/>
+</div>
+</body>
